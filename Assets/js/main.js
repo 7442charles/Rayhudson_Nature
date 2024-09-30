7bfe41b1bc3cbc64ec483dee2191061f24c46c84
7442charles <cking6951@gmail.com>
2024-09-30 13:04:22 +0300

    semi done

---
+++ Assets/js/main.js
@@ -0,0 +1,10 @@
+// init animate on scroll lib
+AOS.init(); 
+
+// nav baar mobile toggle
+const menuButton = document.getElementById('menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
