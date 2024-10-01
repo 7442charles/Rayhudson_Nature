bee2821a204d03f9f07d1b3017361aacc96848eb
7442charles <cking6951@gmail.com>
2024-10-01 14:28:48 +0300

    changed done

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -8,3 +8,21 @@ const mobileMenu = document.getElementById('mobile-menu');
 menuButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 3000,  // Time in milliseconds to wait before changing slides
+        disableOnInteraction: false,  // Allow user interaction to not stop autoplay
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
